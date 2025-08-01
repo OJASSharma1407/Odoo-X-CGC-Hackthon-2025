e634de268811ff14bade1f2cbfb777afc67affce
OJASSharma1407 <ojas1407@gmail.com>
2025-08-01 19:47:53 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+   <>
+    <div className='h-screen text-5xl flex items-center justify-center'>Hello i am frontend :)</div>
+   </>
+  );
+}
+
+export default App;
