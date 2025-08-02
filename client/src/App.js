0883f2f9eff40795ee6cbfe2a86a1ff1c4c476f1
gagandeep kaur <gagandeepkaur@gagandeeps-MacBook-Air.local>
2025-08-02 11:15:34 +0530

    login and signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,37 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/auth/LoginPage";
+import SignupPage from "./pages/auth/SignupPage";
 
 function App() {
   return (
-   <>
-    <div className='h-screen text-5xl flex items-center justify-center'>Hello i am frontend :)</div>
-   </>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
 export default App;
+
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import LoginPage from "./pages/auth/LoginPage";
+// import SignupPage from "./pages/auth/SignupPage";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<LoginPage />} />
+//         <Route path="/signup" element={<SignupPage />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
